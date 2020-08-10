fa2cbe9dbe75def7258ed5c41522a931ce9dc08e
Ankit Rajput <ankitrajput0096@gmail.com>
2020-08-10 18:21:00 +0530

    adding api which will timeout due to high time

--- SpringCloud_API_Gateway_Eureka_SpringBoot/ClientOne/clientone/src/main/java/com/springservice/clientone/controllers/simpleControllerOne.java
+++ SpringCloud_API_Gateway_Eureka_SpringBoot/ClientOne/clientone/src/main/java/com/springservice/clientone/controllers/simpleControllerOne.java
@@ -47,4 +47,17 @@ public class simpleControllerOne {
         .ok()
         .body("This is hello endpoint two from controller one in client One on port : " + (env.getProperty("local.server.port")));
     }
+
+    @RequestMapping(value="/EndpointWithNoResponse", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<String> controllerClientOneEndpointTakesLongTime(@RequestHeader("sso-token") String token) {
+
+        logger.info("Inside service client one, controller one and endpoint which takes more than 10 sec to respond.");
+        logger.info("Now processing sso token passed by api gateway"+token);
+        //Add a small delay in response
+        utils.addDelay(60);
+
+        return ResponseEntity
+                .ok()
+                .body("This is hello endpoint which takes more than 10 sec to respond from controller one in client One on port : " + (env.getProperty("local.server.port")));
+    }
 }
